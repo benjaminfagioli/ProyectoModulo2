1d4afec086dea9861dba42a6a49dcd3a0a30b5b6
benjaminfagioli <benjitafagg@gmail.com>
2023-10-31 19:21:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// const botonComenzar= document.querySelector('.neon')
+// const sectionHero = document.querySelector('.hero')
+// // botonComenzar.addEventListener('click', ()=> {
+// //   setInterval(() => {
+// //     sectionHero.style.display = 'none'
+// //   }, 1000);
+// // })

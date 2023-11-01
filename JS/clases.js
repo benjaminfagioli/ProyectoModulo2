6f49aa81888822c92ed2c0ff4f4b704c3d083043
unknown <martinezesteban284@gmail.com>
2023-11-01 20:37:27 -0300

    pagina administrador

---
+++ JS/clases.js
@@ -0,0 +1,10 @@
+export class Gift {
+    constructor(id, gift, tipo, tiempo, precio, imagen){
+        this.id=id
+        this.gift=gift
+        this.tiempo=tiempo
+        this.tipo=tipo
+        this.precio=precio
+        this.imagen=imagen
+    }
+}

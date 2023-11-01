a8b81d65f0d8081cfb7b2d607e79e3c62dc62ce6
benjaminfagioli <benjitafagg@gmail.com>
2023-11-01 19:39:10 -0300

    juegoDestacado y filtrarPorCategorias

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ carrouselBtns.forEach((punto,i) =>{
 
 const juegoDestacado = document.getElementById('infoFeaturedGame')
 const printFeaturedGame = async (id)=>{
+  juegoDestacado.innerHTML= ''
   let featuredsGame = (await getFeaturedsGame())[id]
   console.log(featuredsGame);
   let gameCard = document.createElement('div')
@@ -64,6 +65,17 @@ const printFeaturedGame = async (id)=>{
 }
 printFeaturedGame(5)
 
+const chooseFeaturedGameToPrint = async () =>{
+  let featuredsGames = await getFeaturedsGame()
+  let array = [] 
+  featuredsGames.forEach((game, i)=> {
+    array.push(`${i}, ${game.name}`)
+  }) 
+  let posicion = prompt(`Ingresa el juego que quieres destacar:\n${array.join('\n')}`)
+  printFeaturedGame(parseInt(posicion))
+}
+document.getElementById('testbtn').addEventListener('click', chooseFeaturedGameToPrint)
+
 let allCategories = new Set
 const getAllCategories = ()=>{
   products.forEach((product, i) =>{
@@ -72,6 +84,40 @@ const getAllCategories = ()=>{
   })
   return Array.from(allCategories).sort()
 }
-getAllCategories()
 
-document.getElementById('testbtn').addEventListener('click', getAllCategories)
+const btnCategories = Array.from(document.getElementsByClassName('categoryBtn'))
+const printCategoriesButtons = ()=> {
+  let botones = btnCategories
+  botones.forEach( boton => {
+    let categorias = getAllCategories()
+    let categoriaAleatoria = categorias[parseInt(Math.random() * (categorias.length))]
+    boton.innerHTML = categoriaAleatoria
+  })
+}
+printCategoriesButtons()
+
+
+const containerCategoryGames = document.getElementById('gamesByCategory')
+
+
+const printCategoryGames = (i)=>{
+  containerCategoryGames.innerHTML=''
+  let botonPulsado = btnCategories[i]
+  let categoriaSeleccionada = botonPulsado.innerText
+  let resultados = products.filter(game => game.categories.includes(categoriaSeleccionada))
+  resultados.forEach((game) => {
+    let gameCard = document.createElement('div')
+    gameCard.innerHTML= `<div class='p-2'> <h5 class=' display-6 fs-3 text-center my-2'> ${game.name} </h5> <p class='ms-2 text-end'>Precio: <b class='text-success'>${game.price}</b></p> </div><img src='${game.images[1]}'>`
+    gameCard.classList= 'card col-md-4 col-lg-2 m-md-3 my-2 d-flex flex-column justify-content-between'
+    containerCategoryGames.appendChild(gameCard)
+  }
+  )
+}
+printCategoryGames(0)
+btnCategories.forEach((boton, i ) => {
+  boton.addEventListener('click', ()=> {
+    printCategoryGames(i) 
+    btnCategories.forEach(boton => boton.classList.remove('categoriaActual'))
+    boton.classList.add('categoriaActual')
+  })
+})
